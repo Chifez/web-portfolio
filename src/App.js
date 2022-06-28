096ff6d88f9d327ab5f46ffeadf22b4fac7abf12
Chifez <Chifez1@gmail.com>
2022-06-28 22:44:43 +0100

    added the loader effect

--- src/App.js
+++ src/App.js
@@ -3,43 +3,50 @@ import AppContext from "./context/context";
 import "./App.css";
 import Home from "./pages/home/home"
 import Navbar from './components/navbar'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import {motion,useViewportScroll,useTransform} from 'framer-motion'
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import {Triangle} from 'react-loader-spinner'
+import {motion,useTransform} from 'framer-motion'
 
 
 function App() {
   const [scrollPos, setScrollPos] = useState(0)
   const [open ,setIsOpen] = useState(false)
+  const [loading,setIsloading] = useState(false)
+
 
   const onScroll = () =>{
     let pixelFromTop = window.scrollY;
-    // console.log(pixelFromTop)
     let documentHeight = document.body.clientHeight;
     let windowHeight= window.innerHeight;
-    // console.log(windowHeight)
     let difference = documentHeight - windowHeight;
     let percentage = (100 * pixelFromTop) / difference;
-    // console.log(percentage) 
     setScrollPos(percentage);
   }
   
-
-  // const {scrollYProgress} = useViewportScroll();
-  
-  // const yRange = useTransform(scrollYProgress, [0, 1], [0, 100])
-  // console.log(yRange)
-
-  useEffect(() =>{
-      window.addEventListener("scroll", onScroll)
+  useEffect (()=>{
+    setIsloading(false);
+    window.addEventListener('scroll', onScroll)
+    setTimeout(()=>{
+      setIsloading(true)},3000)
       return ()=> window.removeEventListener('scroll', onScroll)
-    },[]);
+  },[])
+
+  // useEffect(() =>{
+  //     window.addEventListener('scroll', onScroll)
+  //     return ()=> window.removeEventListener('scroll', onScroll)
+  //   },[]);
 
   return (
               <motion.div className="App">
-                  <AppContext.Provider value = {{scrollPos, open, setIsOpen}}>
+                {!loading && <div 
+                style={{display:'flex', justifyContent:'center', alignItems:'center', width:'100vw', height:'100vh', backgroundColor:'black'}}
+                >
+                  <Triangle color="#00BFFF" height={100} width={100} arialLabel='loading-indicator' />
+                  </div>}
+                  {loading && <AppContext.Provider value = {{scrollPos, open, setIsOpen}}>
                   <Navbar/>
                   <Home />
-                  </AppContext.Provider>
+                  </AppContext.Provider>}
               </motion.div>
 
   );
