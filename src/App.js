4f6bc469bfa9ed27acef1744181325100f6eee94
emmanuel <chifez1@gmail.com>
2023-07-15 14:16:22 +0100

    updated the title and favicon

--- src/App.js
+++ src/App.js
@@ -1,54 +1,63 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import AppContext from "./context/context";
 import "./App.css";
-import Home from "./pages/home/home"
-import Navbar from './components/navbar'
+import Home from "./pages/home/home";
+import Navbar from "./components/navbar";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
-import {Triangle} from 'react-loader-spinner'
-import {motion,useTransform} from 'framer-motion'
-
+import { Triangle } from "react-loader-spinner";
+import { motion, useTransform } from "framer-motion";
 
 function App() {
-  const [scrollPos, setScrollPos] = useState(0)
-  const [open ,setIsOpen] = useState(false)
-  const [loading,setIsloading] = useState(false)
+  const [scrollPos, setScrollPos] = useState(0);
+  const [open, setIsOpen] = useState(false);
+  const [loading, setIsloading] = useState(false);
 
-
-  const onScroll = () =>{
+  const onScroll = () => {
     let pixelFromTop = window.scrollY;
     let documentHeight = document.body.clientHeight;
-    let windowHeight= window.innerHeight;
+    let windowHeight = window.innerHeight;
     let difference = documentHeight - windowHeight;
     let percentage = (100 * pixelFromTop) / difference;
     setScrollPos(percentage);
-  }
-  
-  useEffect (()=>{
-    setIsloading(false);
-    window.addEventListener('scroll', onScroll)
-    setTimeout(()=>{
-      setIsloading(true)},3000)
-      return ()=> window.removeEventListener('scroll', onScroll)
-  },[])
+  };
 
-  // useEffect(() =>{
-  //     window.addEventListener('scroll', onScroll)
-  //     return ()=> window.removeEventListener('scroll', onScroll)
-  //   },[]);
+  useEffect(() => {
+    setIsloading(false);
+    window.addEventListener("scroll", onScroll);
+    setTimeout(() => {
+      setIsloading(true);
+    }, 3000);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
 
   return (
-              <motion.div className="App">
-                {!loading && <div 
-                style={{display:'flex', justifyContent:'center', alignItems:'center', width:'100vw', height:'100vh', backgroundColor:'black'}}
-                >
-                  <Triangle color="#00BFFF" height={120} width={120} arialLabel='loading-indicator' />
-                  </div>}
-                  {loading && <AppContext.Provider value = {{scrollPos, open, setIsOpen}}>
-                  <Navbar/>
-                  <Home />
-                  </AppContext.Provider>}
-              </motion.div>
-
+    <motion.div className="App">
+      {!loading && (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            width: "100vw",
+            height: "100vh",
+            backgroundColor: "black",
+          }}
+        >
+          <Triangle
+            color="#00BFFF"
+            height={120}
+            width={120}
+            arialLabel="loading-indicator"
+          />
+        </div>
+      )}
+      {loading && (
+        <AppContext.Provider value={{ scrollPos, open, setIsOpen }}>
+          <Navbar />
+          <Home />
+        </AppContext.Provider>
+      )}
+    </motion.div>
   );
 }
 
--- src/components/card.js
+++ src/components/card.js
@@ -1,76 +1,68 @@
 import { icons } from './images';
- export const cards = [
-
-     { 
-         className:'work fylo',
-         link:'https://fylo-coral-psi.vercel.app/',
-         title:"fylo",
-         img: icons.github,
-         links:'https://github.com/Chifez/fylo',
-         animate:"fade-up-left",
-         tools:[
-            "react","tailwindcss","firebase"
-         ],
-         description: "Fylo is a simple storage application,that was coined out from building a simple landing page design into a full apllication"
-     },
-     {
-        className:'work weatherApp',
-        link:'https://weatherapp-ten-delta.vercel.app/',
-        title:"weatherApp",
-        img: icons.github,
-        links:'https://github.com/Chifez/weatherapp',
-        animate:"fade-up-right",
-        tools:[
-           "react","CSS"
-        ],
-        description: "A weather apllication that uses a 2 way data fetching system,on initial render it gets the location of its user using the Geolaction API and renders the weather data,also users can search by inputing the desired location"
-    },
-    {
-        className:' work Todo_app',
-        link:'https://note-taking-app-henna.vercel.app/',
-        title:"Todo app",
-        img: icons.github,
-        links:'https://github.com/Chifez/note-taking-app',
-        animate:"fade-up-left",
-        tools:[
-           "react","CSS"
-        ],
-        description: "A markdown enabled note taking application, that has an edit and preview section, users can create,delete and update notes"
-    },
-    {
-        className:'work Expense_tracker',
-        link:'https://expense-tracker-livid-eight.vercel.app/',
-        title:"Expense tracker",
-        img: icons.github,
-        links:'https://github.com/Chifez/expense-tracker',
-        animate:"fade-up-right",
-        tools:[
-           "javascript","CSS","HTML"
-        ],
-        description: "A basic Application that can be used to track incomes and spendings"
-    },
-    {
-        className:'work tic_tac_toe',
-        link:'https://tic-tac-toe-chifez.vercel.app/',
-        title:"Tic tac toe",
-        img: icons.github,
-        links:'https://github.com/Chifez/tic-tac-toe',
-        animate:"fade-up-left",
-        tools:[
-           "javascript","CSS","HTML"
-        ],
-        description: "A fun tic-tac-toe web apllication with a good UI"
-    },
-    {
-      className:'work guessing_game',
-      link:'https://guess-it.vercel.app/',
-      title:"guessing game",
-      img: icons.github,
-      links:'https://github.com/Chifez/guess-it',
-      animate:'fade-up-right',
-      tools:[
-         "javascript","CSS","HTML"
-      ],
-      description: "A simple guessing game where users are given 20 tries to guess a secret number"
+export const cards = [
+  {
+    className: 'work faloni',
+    link: 'https://stylbit.vercel.app/',
+    title: 'faloni',
+    img: icons.github,
+    links: 'https://github.com/Chifez/stylbit',
+    animate: 'fade-up-left',
+    tools: ['React', 'typescript', 'Firebase'],
+    description:
+      'Faloni is an e-commerce store,built using typescript,Redux toollkit,stripe for payment gateway and tailwindcss for styling.',
   },
- ]
+  {
+    className: 'work fylo',
+    link: 'https://fylo-coral-psi.vercel.app/',
+    title: 'fylo',
+    img: icons.github,
+    links: 'https://github.com/Chifez/fylo',
+    animate: 'fade-up-right',
+    tools: ['React', 'tailwindcss', 'Firebase'],
+    description:
+      'Fylo is a simple storage application,that was coined out from building a simple landing page design into a full apllication',
+  },
+  {
+    className: 'work weatherApp',
+    link: 'https://weatherapp-ten-delta.vercel.app/',
+    title: 'weatherApp',
+    img: icons.github,
+    links: 'https://github.com/Chifez/weatherapp',
+    animate: 'fade-up-left',
+    tools: ['React', 'Javascript'],
+    description:
+      'A weather apllication that uses a 2 way data fetching system,on initial render it gets the location of its user using the Geolaction API and renders the weather data,also users can search by inputing the desired location',
+  },
+  {
+    className: ' work Todo_app',
+    link: 'https://note-taking-app-henna.vercel.app/',
+    title: 'Todo app',
+    img: icons.github,
+    links: 'https://github.com/Chifez/note-taking-app',
+    animate: 'fade-up-right',
+    tools: ['React', 'Javascript'],
+    description:
+      'A markdown enabled note taking application, that has an edit and preview section, users can create,delete and update notes',
+  },
+  {
+    className: 'work tic_tac_toe',
+    link: 'https://tic-tac-toe-chifez.vercel.app/',
+    title: 'Tic tac toe',
+    img: icons.github,
+    links: 'https://github.com/Chifez/tic-tac-toe',
+    animate: 'fade-up-left',
+    tools: ['Javascript', 'CSS', 'HTML'],
+    description: 'A fun tic-tac-toe web apllication with a good UI',
+  },
+  {
+    className: 'work guessing_game',
+    link: 'https://guess-it.vercel.app/',
+    title: 'guessing game',
+    img: icons.github,
+    links: 'https://github.com/Chifez/guess-it',
+    animate: 'fade-up-right',
+    tools: ['Javascript', 'CSS', 'HTML'],
+    description:
+      'A simple guessing game where users are given 20 tries to guess a secret number',
+  },
+];
